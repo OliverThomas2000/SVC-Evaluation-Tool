f26bdd602780a7d5f1318303316d74774e5ab979
ollie-thomas <oliverjthomas2000@gmail.com>
2023-01-13 15:38:25 +0000

    Displays the Euclidean Distance between the two sets of MFCCs on the UI

--- UI.py
+++ UI.py
@@ -8,15 +8,15 @@ class App:
     def __init__(self, root):
         self.root = root
         self.root.title("SVC Evaluation Tool")
-        self.root.config(bg='black')
+        self.root.config(bg='white')
 
         self.main_frame = tk.Frame(self.root, width=1000, height=400, bg='white')
         self.main_frame.grid(row=0, column=0, padx=20, pady=10)
 
-        self.target_frame = tk.Frame(self.main_frame, width=450, height=400, bg='grey')
+        self.target_frame = tk.Frame(self.main_frame, width=450, height=400, bg='white')
         self.target_frame.grid(row=0,column=0,padx=50,pady=0)
 
-        self.converted_frame = tk.Frame(self.main_frame, width=450, height=400, bg='grey')
+        self.converted_frame = tk.Frame(self.main_frame, width=450, height=400, bg='white')
         self.converted_frame.grid(row=0,column=1,padx=50,pady=0)
 
         size = (5,5)
@@ -37,7 +37,7 @@ class App:
 
         self.upload_target = tk.Button(self.target_frame, text="Upload Target Audio", 
             command=lambda: self.upload_audio("target"))
-        self.upload_converted = tk.Button(self.converted_frame, text="Upload Converted Audio", 
+        self.upload_converted = tk.Button(self.converted_frame, text="Upload Model Output", 
             command=lambda: self.upload_audio("converted"))
 
         self.upload_target.pack()
@@ -48,6 +48,10 @@ class App:
 
         self.calculate_difference_button = tk.Button(self.options_frame, text="Calculate Euclidean difference", command=self.calculate_difference)
         self.calculate_difference_button.pack()
+
+        self.difference = tk.StringVar(self.options_frame, value= 'Difference: 0.00000')
+        self.difference_label = tk.Label(self.options_frame, textvariable=self.difference, font=('Arial', 25), bg='white')
+        self.difference_label.pack()
 
     def upload_audio(self, button):
         file_path = filedialog.askopenfilename(filetypes=[("WAV file", ".wav")])
@@ -72,9 +76,11 @@ class App:
             mfcc1 = self.ax1.images[-1].get_array()
             mfcc2 = self.ax2.images[-1].get_array()
         except:
-            tk.messagebox.showerror("Error", "Please upload target and converted audio first")
+            tk.messagebox.showerror("Error", "Please upload target and model output audio first")
             return
         difference = mfcc_difference(mfcc1,mfcc2)
+        print(f"Difference: {difference:.5f}")
+        self.difference.set(f"Difference: {round(difference, 5)}")
 
 if __name__=="__main__":
     root = tk.Tk()
